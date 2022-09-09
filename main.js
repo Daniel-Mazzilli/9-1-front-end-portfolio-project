f457afe85bbd168075818606e562cc9a41124de6
Daniel-Mazzilli <danmazzilli@pursuit.org>
2022-09-09 11:11:31 -0400

    small edits

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-let url = `https://collectionapi.metmuseum.org/public/collection/v1/objects/247001`
+let exampleURL = `https://collectionapi.metmuseum.org/public/collection/v1/objects/247001`
 
-fetch(url)
+fetch(exampleURL)
   .then((res) => res.json())
   .then((resJson) => console.log(resJson))
   .catch((err) => console.log(err));
