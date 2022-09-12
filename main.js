e47f5bd6736f4d2431cb5cc680184a70c0fb2c61
Daniel-Mazzilli <danmazzilli@pursuit.org>
2022-09-12 12:09:52 -0400

    minor edits

--- main.js
+++ main.js
@@ -1,31 +1,45 @@
-// Build URL for Fetch
+// DOM Elements
+const h2 = document.querySelector(`h2`);
+const hiddenAtFirst = document.querySelectorAll(`.hidden-at-first`);
+const header = document.querySelector(`header`);
+const form = document.querySelector(`form`);
+
+// Variables
+let totalItems;
+let itemsIds;
+let randomIndex;
+let randomId;
+
+// URL for Fetch
 const exampleURL = `https://collectionapi.metmuseum.org/public/collection/v1/objects/247001`;
 const rootURL = `https://collectionapi.metmuseum.org/public/collection/v1/`;
 
-// Total Items
-const h2 = document.querySelector(`h2`);
-let totalItems;
-
+// Initial Fetch for Total Items
 fetch(`${rootURL}objects`)
   .then((res) => res.json())
   .then((resJson) => {
     totalItems = resJson.total;
-    console.log(totalItems);
+    itemsIds = resJson.objectIDs;
+
+    // Update look and style
     h2.innerHTML = `Gain access to and explore ${totalItems} artifacts`;
-    const hiddenAtFirst = document.querySelectorAll(`.hidden-at-first`);
     hiddenAtFirst.forEach((el) => {
       el.classList.remove(`hidden-at-first`);
-      console.log(el);
     });
     document.body.classList.remove(`initial-look`);
-    const header = document.querySelector(`header`);
     header.classList.add(`new-look`);
-    // Random Item
-    console.log(`first`, (totalItems * Math.random()).toFixed());
   })
   .catch((err) => console.log(err));
 
+// Example Fetch
 fetch(exampleURL)
   .then((res) => res.json())
-  .then((resJson) => console.log(`JS connected`))
+  .then((resJson) => console.log(resJson.objectID))
   .catch((err) => console.log(err));
+
+// Functions
+const randomIndexForObj = () => {
+  randomIndex = Math.round(Math.random() * totalItems);
+};
+
+// Search fetch
---
+++ testing.js
@@ -0,0 +1,3 @@
+for (let i = 0; i < 15; i++) {
+  console.log(Math.round(Math.random() * 481212));
+}
