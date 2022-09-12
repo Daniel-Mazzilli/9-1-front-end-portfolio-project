9b3cee05d8b40b861369bf3879bff787e6f1e1e1
Daniel-Mazzilli <danmazzilli@pursuit.org>
2022-09-12 01:49:20 -0400

    look and style edits

--- main.js
+++ main.js
@@ -1,6 +1,31 @@
-let exampleURL = `https://collectionapi.metmuseum.org/public/collection/v1/objects/247001`
+// Build URL for Fetch
+const exampleURL = `https://collectionapi.metmuseum.org/public/collection/v1/objects/247001`;
+const rootURL = `https://collectionapi.metmuseum.org/public/collection/v1/`;
+
+// Total Items
+const h2 = document.querySelector(`h2`);
+let totalItems;
+
+fetch(`${rootURL}objects`)
+  .then((res) => res.json())
+  .then((resJson) => {
+    totalItems = resJson.total;
+    console.log(totalItems);
+    h2.innerHTML = `Gain access to and explore ${totalItems} artifacts`;
+    const hiddenAtFirst = document.querySelectorAll(`.hidden-at-first`);
+    hiddenAtFirst.forEach((el) => {
+      el.classList.remove(`hidden-at-first`);
+      console.log(el);
+    });
+    document.body.classList.remove(`initial-look`);
+    const header = document.querySelector(`header`);
+    header.classList.add(`new-look`);
+    // Random Item
+    console.log(`first`, (totalItems * Math.random()).toFixed());
+  })
+  .catch((err) => console.log(err));
 
 fetch(exampleURL)
   .then((res) => res.json())
-  .then((resJson) => console.log(resJson))
+  .then((resJson) => console.log(`JS connected`))
   .catch((err) => console.log(err));
